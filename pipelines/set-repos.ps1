10ca974c4f2bb4db741f2f26c0ed6bc93fb408a0
Matt Monks <mals2003@hotmail.com>
2023-11-23 22:16:42 +0000

    Added script to set up repository on environment

---
+++ pipelines/set-repos.ps1
@@ -0,0 +1,32 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $FactoryName,
+
+    [Parameter(Mandatory = $true)]
+    [string] $RepositoryAccount,
+
+    [Parameter(Mandatory = $true)]
+    [string] $RepositoryName
+)
+
+$factories = az datafactory list | ConvertFrom-Json
+$account = az account show | ConvertFrom-Json
+
+
+
+foreach($factory in $factories)
+{
+    $dfName = $factory.name
+    $dfId = $factory.id
+    $tenantId = $account.tenantId
+    $location = $factory.location
+    $rgName = $factory.resourceGroup
+
+    if($factory.name.EndsWith($FactoryName))
+    {
+        az datafactory configure-factory-repo `
+            --factory-resource-id "$dfId" `
+            --location "$location" `
+            --factory-git-hub-configuration account-name="$RepositoryAccount" repository-name="$RepositoryName" root-folder="/pipelines" collaboration-branch="adf"
+    }
+}
